93a07c5d15c40e31d062e1b56a3f77df8d7b037d
Ana Luiza Elert <107824361+AnaLuizaElert@users.noreply.github.com>
2023-11-08 21:59:52 -0300

    errors and errors

---
+++ test.py
@@ -0,0 +1,35 @@
+# from NLTK import wv
+#
+# pairs = [
+#     'gear',
+#     'reducers',
+#     # 'engines',
+#     'motors',
+#     'panels',
+#     'paints',
+#     'pumps',
+#     'energy',
+#     'mobility',
+#     'software',
+#     'agro',
+#     'cameras',
+#     'controls',
+#     'sensors',
+#     'relays',
+#     'switches',
+#     'plugs'
+# ]
+#
+# # maxSimilarity = 0
+# similarities = []
+# # for w1 in pairs:
+# #     print(w1)
+# #     for w2 in pairs:
+# #         if w1 is not w2:
+# #             # if wv.similarity(w1, w2) > maxSimilarity:
+# #             #     maxSimilarity = wv.similarity(w1, w2)
+# #             similarities.append([w1, w2, wv.similarity(w1, w2)])
+# #             # print('%r\t%r\t%.2f' % (w1, w2, wv.similarity(w1, w2)))
+#
+# # print(maxSimilarity)
+# print(similarities)
